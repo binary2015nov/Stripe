a631ce0f51c711d0b0b41264b87a855cb8b7d93a
申行 <binary2015nov@outlook.com>
2017-10-13 10:03:05 +0800

    update dlls

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,6 @@
-SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
 REM %MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
 REM %MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
 
-msbuild /p:Configuration=Release ..\src\Stripe.sln
+%msbuild% /p:Configuration=Release ..\src\Stripe.sln
